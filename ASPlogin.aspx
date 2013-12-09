e3461d0d457184c06279ed3cdb4e34c730ba1844
DevinLevy <analescheok@hotmail.com>
2013-12-09 01:40:57 -0700

    commit2

---
+++ ASPlogin.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="ASPlogin.aspx.cs" Inherits="ASPlogin" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <style type="text/css">
+        form {
+            background-image: url("Images/psychology-pic.jpg");
+            width: 900px;
+            height: 300px;
+        }
+        .formStyle {
+            border: double blue;
+            outline-color:blue;
+            margin-top:0;
+            margin-left:507px;
+            background-color:#90a6ec;
+            opacity:0.9;
+            height:300px;
+            position:relative;
+           
+        }
+        div {
+            margin-top: 0px;
+        }
+         h2 {
+        font-family: 'Browallia New';
+        color: blue;
+        text-align:center;
+    }
+        .text {
+            
+            border: double blue;
+            color:black;
+        }
+        text.hover {
+                    transform: scale(1.2, 1.2);
+        -moz-transform: scale(1.2, 1.2);
+        -ms-transform: scale(1.2, 1.2);
+        -webkit-transform: scale(1.2, 1.2);
+        -o-transform: scale(1.2, 1.2);
+        background-color: azure;
+        opacity: 0.5;
+        
+    }
+
+    </style>
+<head runat="server">
+   
+        <h2>
+Welcome to Psychology Research Methods
+   </h2>
+</head>
+<body>
+     <asp:HyperLink ID="HyperLink1" CssClass="text" runat="server" NavigateUrl="~/Register.aspx" ToolTip="Click this button if you would like to create an account">Register</asp:HyperLink>
+
+    <form id="form1" runat="server">
+       
+    <div>
+        <asp:Login ID="Login1" CssClass =" formStyle" runat="server" OnAuthenticate="Login1_Authenticate1" Height="221px" Width="300px">
+        </asp:Login>
+    </div>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    </form>
+</body>
+</html>
---
+++ ASPlogin.aspx.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Authenticate;
+
+public partial class ASPlogin : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Login1_Authenticate1(object sender, AuthenticateEventArgs e)
+    {
+        //if (Membership.ValidateUser(Login1.UserName, Login1.Password))
+        //{
+        //    Session["UserName"] = Login1.UserName;
+        //    Session["Password"] = Login1.Password;
+        //    e.Authenticated = true;
+        //    if (Roles.IsUserInRole("student"))
+        //    {
+        //        Login1.DestinationPageUrl = "FlexBox.aspx";
+        //        Session["role"] = "student";
+        //    }
+
+        //    else if (Roles.IsUserInRole("proff"))
+        //        Login1.DestinationPageUrl = "Default.aspx";
+       // }
+        string username = Login1.UserName;
+        string password = Login1.Password;
+        string toSend = username + "/" + password;
+        //try
+        //{
+        //    TcpClient tcpClient = new TcpClient();
+        //    Console.WriteLine("Connecting.....");
+        //    ASCIIEncoding asen = new ASCIIEncoding();
+        //    string myIp = Dns.GetHostEntry(Dns.GetHostName()).AddressList.Where(ip => ip.AddressFamily == AddressFamily.InterNetwork).Select(ip => ip.ToString()).FirstOrDefault() ?? "";
+        //    tcpClient.Connect(myIp, 8001);
+
+             
+        //    Stream stm = tcpClient.GetStream();
+            
+
+        //    byte[] auth = asen.GetBytes(toSend);
+        //    stm.Write(auth, 0, auth.Length);
+       
+        //    byte[] answer = new byte[100];
+        //    int a = stm.Read(answer, 0, 100);
+        //    char[] array = new char[a];
+           
+        //    for (int x = 0; x < a; x++)
+        //    {
+        //        array[x] = Convert.ToChar(answer[x]);
+
+        //    }
+        //    string reply = new string(array);
+        //    TextBox1.Text = reply;
+        //    if (reply == "true")
+        //    {
+        //        Session["UserName"] = Login1.UserName;
+        //        Session["Password"] = Login1.Password;
+        //        e.Authenticated = true;
+        //        Login1.DestinationPageUrl = "FlexBox.aspx";
+        //       // Response.Redirect("FlexBox.aspx");
+        //        Session["role"] = "student";
+                
+
+        //    }
+        //    tcpClient.Close();
+           
+        //}
+
+        //catch (Exception ex)
+        //{
+            
+        //}
+        Authenticate.Service1Client   proxy = new Service1Client();
+        bool auth = proxy.CheckTheLogIn(username, password);
+        if (auth)
+        {
+            Session["UserName"] = Login1.UserName;
+            Session["Password"] = Login1.Password;
+            e.Authenticated = true;
+            Login1.DestinationPageUrl = "FlexBox.aspx";
+            // Response.Redirect("FlexBox.aspx");
+            Session["role"] = "student";
+        }
+
+    }
+   
+
+
+
+}
+
+
+
+
